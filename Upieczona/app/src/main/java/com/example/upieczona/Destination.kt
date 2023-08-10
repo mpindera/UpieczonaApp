254c043e87029fe7bb3b4ec6d1175b5c9a1e7d41
mpindera <mikolaj.pindera@op.pl>
2023-08-11 00:36:05 +0200

    Fix

--- Upieczona/app/src/main/java/com/example/upieczona/Destination.kt
+++
@@ -1,7 +0,0 @@
-package com.example.upieczona
-
-sealed class Destination(val route: String) {
-    object HomePage : Destination("MainScreenUpieczona")
-    object ContentPage : Destination("Content")
-    object MainPageOfUpieczona : Destination("MainScreenOfUpieczonaToRefresh")
-}
--- Upieczona/app/src/main/java/com/example/upieczona/dto/PostsOfUpieczona.kt
+++
@@ -1,7 +0,0 @@
-package com.example.upieczona.dto
-
-
-import com.google.gson.annotations.SerializedName
-import androidx.annotation.Keep
-
-class PostsOfUpieczona : ArrayList<PostsOfUpieczonaItemDto>()
